c624a8b1b76d346e79d62602519416c5430f4115
Huzaifa_q <huzaifaquadri1853@gmail.com>
2024-07-04 09:28:19 +0530

    Firebase CLI Added

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -27,78 +27,80 @@ class _AuthenticateScreenState extends State<AuthenticateScreen> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              margin: const EdgeInsets.only(
-                top: 30,
-                bottom: 20,
-                left: 20,
-                right: 20,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: const EdgeInsets.only(
+                  top: 30,
+                  bottom: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                width: 200,
+                child: Image.asset('assets/images/chaticon.png'),
               ),
-              width: 200,
-              child: Image.asset('assets/images/chaticon.png'),
-            ),
-            Card(
-              margin: const EdgeInsets.all(20),
-              child: SingleChildScrollView(
-                child: Padding(padding: const EdgeInsets.all(16),
-                  child: Form(
-                    key: _form,
-            
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [                                                  
-                        TextFormField(                                                                           //!  Email Textfield                                                      
-                          decoration: const InputDecoration(
-                            labelText: 'Email Address'
+              Card(
+                margin: const EdgeInsets.all(20),
+                child: SingleChildScrollView(
+                  child: Padding(padding: const EdgeInsets.all(16),
+                    child: Form(
+                      key: _form,
+              
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [                                                  
+                          TextFormField(                                                                           //!  Email Textfield                                                      
+                            decoration: const InputDecoration(
+                              labelText: 'Email Address'
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            validator: (value) {
+                              if(value == null || value.trim().isEmpty || !value.contains('@')){
+                                return 'Enter a valid e-mail Address';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) => _email_id = value.toString(),
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                          validator: (value) {
-                            if(value == null || value.trim().isEmpty || !value.contains('@')){
-                              return 'Enter a valid e-mail Address';
-                            }
-                            return null;
-                          },
-                          onSaved: (value) => _email_id = value.toString(),
-                        ),
-                        TextFormField(                                                                                //!   Password Textfield
-                          decoration: const InputDecoration(
-                            labelText: 'Password'
+                          TextFormField(                                                                                //!   Password Textfield
+                            decoration: const InputDecoration(
+                              labelText: 'Password'
+                            ),
+                            obscureText: true,
+                            validator: (value) {
+                            if(value == null || value.trim().length < 6 ){
+                                return 'Enter a valid Password';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) => _password = value.toString(),
                           ),
-                          obscureText: true,
-                          validator: (value) {
-                          if(value == null || value.trim().length < 6 ){
-                              return 'Enter a valid Password';
-                            }
-                            return null;
-                          },
-                          onSaved: (value) => _password = value.toString(),
-                        ),
-                        const SizedBox(height: 16,),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primaryContainer),
-                          onPressed: _onpressedsave, 
-                          child: Text(_isLogin ? 'Login' : "Sign Up"),
-                        ),
-                        ElevatedButton(
-                          onPressed: (){
-                            setState(() {
-                              _isLogin = !_isLogin; //* if _islogin == true => it will yeild false & if _islogin == false => it will yeild true
-                            });
-                          }, 
-                          child: Text(_isLogin ? "Create an Account or Login" : 'I already have an account.'),
-                        ),
-                      ],
+                          const SizedBox(height: 16,),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.primaryContainer),
+                            onPressed: _onpressedsave, 
+                            child: Text(_isLogin ? 'Login' : "Sign Up"),
+                          ),
+                          ElevatedButton(
+                            onPressed: (){
+                              setState(() {
+                                _isLogin = !_isLogin; //* if _islogin == true => it will yeild false & if _islogin == false => it will yeild true
+                              });
+                            }, 
+                            child: Text(_isLogin ? "Create an Account or Login" : 'I already have an account.'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
