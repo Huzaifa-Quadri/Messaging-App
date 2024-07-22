3e010423db0db833225c3952ae889e29e735dffa
Huzaifa_q <huzaifaquadri1853@gmail.com>
2024-07-22 18:13:08 +0530

    Retrieving data and displaying messages on Screen

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,14 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 
-class MessageScreen extends StatelessWidget{
+class MessageScreen extends StatelessWidget {
   const MessageScreen({super.key});
 
-  
-   @override
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("No Messages Yet..."),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('time', descending: false)
+          .snapshots(),
+      builder: (context, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
 
+        if (!chatSnapshot.hasData || chatSnapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text("No Messages Yet..."),
+          );
+        }
+
+        if (chatSnapshot.hasError) {
+          return const Center(child: Text("Something went wrong..."));
+        }
+
+        final loadedmessages = chatSnapshot.data!.docs;
+        return ListView.builder(
+          itemCount: loadedmessages.length,
+          itemBuilder: (context, index) => Text(loadedmessages[index].data()['text'])
+        );
+      },
     );
   }
 }
--- lib/widgets/new_message.dart
+++ lib/widgets/new_message.dart
@@ -1,60 +1,77 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+  import 'package:cloud_firestore/cloud_firestore.dart';
+  import 'package:firebase_auth/firebase_auth.dart';
+  import 'package:flutter/material.dart';
 
-class NewMessage extends StatefulWidget {
-  const NewMessage({super.key});
+  class NewMessage extends StatefulWidget {
+    const NewMessage({super.key});
 
-  @override
-  State<NewMessage> createState() => _NewMessageState();
-}
-
-class _NewMessageState extends State<NewMessage> {
-  final _messagecontroller = TextEditingController();
-
-  @override
-  void dispose(){
-    _messagecontroller.dispose();
-    super.dispose();
+    @override
+    State<NewMessage> createState() => _NewMessageState();
   }
 
+  class _NewMessageState extends State<NewMessage> {
+    final _messagecontroller = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-
-
-    void sendMessage(){
-      final sentmessage = _messagecontroller.text;
-      if (sentmessage.trim().isEmpty) {
-        return;
-      }
-      //Todo : Message Sent
-      
-      _messagecontroller.clear();  
+    @override
+    void dispose(){
+      _messagecontroller.dispose(); 
+      super.dispose();
     }
 
-    return Padding(
-      padding: const EdgeInsets.only(left: 10,right: 5, bottom: 14),
-      child: Row(
-        children: [
-          Expanded(
-            child: TextField(
-              decoration: const InputDecoration(
-                label: Text('Send Message...'),
+
+    @override
+    Widget build(BuildContext context) {
+
+
+      void sendMessage()async{
+        final sentmessage = _messagecontroller.text;
+
+        if (sentmessage.trim().isEmpty) {
+          return;
+        }
+
+        FocusScope.of(context).unfocus();
+        print("Keyboard down");
+        //Todo : Message Sent
+
+        final user = FirebaseAuth.instance.currentUser!;   //* Provide us with info of currently logged in user
+        final userinfo = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+
+        print("data is sending");
+        FirebaseFirestore.instance.collection('chat').add({
+          'text' : sentmessage,
+          'time' : Timestamp.now(),
+          'user_id' : user.uid,
+          'username' : userinfo.data()!['username'],
+          'userimage' : userinfo.data()!['image_url']
+        });
+        print("data is sent");
+
+        _messagecontroller.clear();  
+      }
+
+      return Padding(
+        padding: const EdgeInsets.only(left: 10,right: 5, bottom: 14),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(
+                  label: Text('Send Message...'),
+                ),
+                textCapitalization: TextCapitalization.characters,
+                enableSuggestions: true,
+                controller: _messagecontroller,
               ),
-              textCapitalization: TextCapitalization.characters,
-              enableSuggestions: true,
-              controller: _messagecontroller,
             ),
-          ),
-          IconButton(
-            onPressed: sendMessage,
-            icon: Icon(Icons.send,
-            color: Theme.of(context).colorScheme.primary,
-            ),
-          )
-        ],
-      ),
-    );
+            IconButton(
+              onPressed: sendMessage,
+              icon: Icon(Icons.send,
+              color: Theme.of(context).colorScheme.primary,
+              ),
+            )
+          ],
+        ),
+      );
+    }
   }
-}
