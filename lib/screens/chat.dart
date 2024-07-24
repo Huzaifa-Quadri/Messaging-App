29327807cff3d3f89b48ae670156cac94c73d303
Huzaifa_q <huzaifaquadri1853@gmail.com>
2024-07-24 10:55:29 +0530

    Setting Up and Sending Push Notification

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,10 +1,32 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+
 import 'package:messaging_app/screens/message_screen.dart';
 import 'package:messaging_app/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupnotification() async{
+    final req = FirebaseMessaging.instance;
+    await req.requestPermission();
+
+    final token = await req.getToken();
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupnotification();
+  }
 
   @override
   Widget build(BuildContext context) {
